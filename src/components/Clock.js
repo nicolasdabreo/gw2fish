c1c6ca8bd4b39a74b60ebb67bc330ad74638a77c
Nicolas Dabreo <nickj.dabreo@gmail.com>
2022-03-19 17:37:20 +0000

    Feat: Better time calculation I promise

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react'
 
-import { tyriaTime, tyriaTimeOfDay } from '../helpers/time'
+import { tyriaTime, tyriaTimeOfDay, formatTime } from '../helpers/time'
 
 export default function Clock() {
-  const [date, setDate] = useState(tyriaTime())
+  const [time, setTime] = useState(tyriaTime())
   const [timeOfDay, setTimeOfDay] = useState(tyriaTimeOfDay())
 
   function refreshClock() {
-    setDate(tyriaTime())
+    setTime(tyriaTime())
     setTimeOfDay(tyriaTimeOfDay())
   }
 
   useEffect(() => {
-    const timerId = setInterval(refreshClock, 1000)
+    const timerId = setInterval(refreshClock, 5000)
     return function cleanup() {
       clearInterval(timerId)
     }
@@ -20,7 +20,7 @@ export default function Clock() {
 
   return (
     <div className='flex flex-col'>
-      <time className='text-6xl'>{date}</time>
+      <time className='text-6xl'>{formatTime(time)}</time>
       <time className='text-xl'>{timeOfDay}</time>
     </div>
   )

233330bc782448346099c2b4e32f62fe3d60597a
Nicolas Dabreo <nickj.dabreo@gmail.com>
2022-04-15 21:46:59 +0200

    Add swr and bmac

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,10 @@ export default function Document () {
     <Html>
       <Head>
         <link href='https://fonts.googleapis.com/css?family=PT+Serif:400,700|Open+Sans:300,400,600,700&display=swap' rel='stylesheet' type='text/css' />
+        {
+          process.env.ENV !== 'dev' &&
+            <script data-name='BMC-Widget' data-cfasync='false' src='https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js' data-id='dabreo' data-description='Support me on Buy me a coffee!' data-message='' data-color='#5F7FFF' data-position='Right' data-x_margin='18' data-y_margin='18' />
+        }
       </Head>
       <body>
         <Main />

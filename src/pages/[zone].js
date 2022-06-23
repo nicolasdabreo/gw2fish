00b6276e334ed2d9464976e32978ae0b3dab44d9
Nicolas Dabreo <nickj.dabreo@gmail.com>
2022-06-23 22:04:32 +0100

    Update packages, Use icons, Improve settings menu

--- src/pages/[zone].js
+++ src/pages/[zone].js
@@ -3,6 +3,9 @@ import useSWR from 'swr'
 import Image from 'next/image'
 import { useLocalStorage } from '@rehooks/local-storage'
 
+import { ClockIcon, CogIcon, LocationMarkerIcon } from '@heroicons/react/outline'
+import { MoonIcon, SunIcon } from '@heroicons/react/solid'
+import { Menu, Transition } from '@headlessui/react'
 import Layout from '../components/Layout'
 import Seo from '../components/Seo'
 import Switch from '../components/Switch'
@@ -11,6 +14,11 @@ import Clock from '../components/Clock'
 import client from '../gw2client'
 import { DATA } from '../data'
 import { tyriaTimeOfDay, fetcher, underscore, zipAndMerge } from '../utils'
+
+const TIMEZONES = {
+  tyria: "Tyria",
+  cantha: "Cantha"
+}
 
 export async function getStaticPaths() {
   const paths = Object.keys(DATA).map((zoneSlug) => ({
@@ -39,51 +47,89 @@ export default function Zone ({ zone, achievements }) {
   const [showCaught, setShowCaught] = useState(false)
   const [storedApiKey] = useLocalStorage('gw2f.api_key')
   const { data } = useSWR(storedApiKey ? `https://api.guildwars2.com/v2/account/achievements?access_token=${storedApiKey}&ids=${zone.achievements.join(',')}` : null, fetcher, {fallbackData: null})
+
+
   const timeOfDay = tyriaTimeOfDay()
 
   return (
-    <Layout title={zone.name}>
+    <Layout>
       <Seo />
 
-      <section className='flex flex-row items-end justify-between m-10 text-right layout'>
-        <div className='flex flex-col justify-between h-full'>
-          <div className='flex flex-col mb-2 space-x-0 space-y-2 md:space-x-4 md:space-y-0 md:flex-row md:mb-0'>
-            <Switch
-              label='Show separately'
-              value={showSeparately}
-              handleToggle={setShowSeparately}
-            />
-            {storedApiKey && (
-              <Switch
-                label='Show caught'
-                value={showCaught}
-                handleToggle={setShowCaught}
-              />
-            )}
-          </div>
-        </div>
-
-        <Clock />
+      <section className='flex flex-col justify-end m-10 sm:flex-row layout'>
+        <Clock timezone={TIMEZONES.tyria} />
       </section>
 
       <section className='flex flex-col m-10 text-left layout'>
-        <ul role='list' className='grid grid-cols-1 gap-5 mt-3 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+        <div className='flex flex-row justify-between'>
+          <h1 className='text-xl leading-7 sm:text-2xl sm:truncate'>{zone.name}</h1>
+
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <Menu.Button as="div" className="px-2 py-1 rounded-md cursor-pointer hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-200 focus:ring-indigo-500">
+                <CogIcon className="inline-flex justify-center w-5 h-5 text-sm font-medium text-gray-700 " />
+              </Menu.Button>
+            </div>
+
+            <Transition
+              as={React.Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-50 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="px-1 py-2">
+                  <Menu.Item>
+                    <div className="px-3 py-2">
+                      <Switch
+                        label='Show separately'
+                        value={showSeparately}
+                        handleToggle={setShowSeparately}
+                      />
+                    </div>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <div className="px-3 py-2">
+                      <Switch
+                        label='Show caught'
+                        value={showCaught}
+                        handleToggle={setShowCaught}
+                      />
+                    </div>
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
+        
+        <ul role='list' className='grid grid-cols-1 gap-5 mt-4 sm:gap-6 sm:grid-cols-2 lg:grid-cols-3'>
           {
             maybeFilterCaught(showCaught, zone.fish, achievements,data)
               .map(f => (
                 <li key={f.name} className={`${['Any', timeOfDay].includes(f.time) ? '' : 'opacity-50'} bg-white flex col-span-1 rounded-md shadow-sm`}>
-                  <div className='flex items-center justify-between flex-1 truncate border-t border-b border-r border-gray-200 rounded-md'>
-                    <div className='flex self-center pl-2'>
-                      <Image src={f.icon} className={`flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium aspect-square item-${f.rarity.toLowerCase()}`} height='64' width='64' />
+                  <div className='flex flex-row justify-between w-full border-t border-b border-r border-gray-200 rounded-md'>
+                    <div className='flex items-center justify-between flex-1 truncate'>
+                      <div className='flex self-center pl-2'>
+                        <Image src={f.icon} className={`flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium aspect-square item-${f.rarity.toLowerCase()}`} height='64' width='64' />
+                      </div>
+                      <div className='flex-1 px-4 py-2 text-sm truncate'>
+                        <a href={`https://wiki.guildwars2.com/wiki/${underscore(f.name)}`} className='font-medium text-gray-900 hover:text-gray-600'>
+                          {f.name}
+                        </a>
+                        <p className='text-gray-500'><img src="/bait.png" className='inline-block w-4 h-4 ml-[-2px] mr-2 opacity-60' />{f.bait}</p>
+                        <p className='text-gray-500'><LocationMarkerIcon className='inline-block w-5 h-5 ml-[-4px]' /> {f.hole}</p>
+                      </div>
+                      <div className='flex-shrink-0 pr-2' />
                     </div>
-                    <div className='flex-1 px-4 py-2 text-sm truncate'>
-                      <a href={`https://wiki.guildwars2.com/wiki/${underscore(f.name)}`} className='font-medium text-gray-900 hover:text-gray-600'>
-                        {f.name}
-                      </a>
-                      <p className='text-gray-500'>{f.bait}</p>
-                      <p className='text-gray-500'>{f.hole}</p>
+
+
+                    <div className='flex px-4 py-2 text-sm text-left truncate'>
+                      <ClockIcon className='w-5 h-5 mr-2' />
+                      {f.time}
                     </div>
-                    <div className='flex-shrink-0 pr-2' />
                   </div>
                 </li>
               )
